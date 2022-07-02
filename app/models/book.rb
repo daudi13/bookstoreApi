3823c35d6b21dfcaf519a029c25224a6e42cd454
daudi13 <davouma13@gmail.com>
2022-07-02 20:53:08 +0300

    git add validation for Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, allow_blank: false, length: { maximum: 50}
+  validates :author, presence: true, allow_blank: false, length: { maximum: 50}
+  validates :genre, presence: true, allow_blank: false, length: { maximum: 50}
+  validates :current_chapter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :total_chapters, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+
 end
