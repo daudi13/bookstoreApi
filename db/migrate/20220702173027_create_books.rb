5d6c5ba5bf9329a9743d19df4aa5986b665515ab
daudi13 <davouma13@gmail.com>
2022-07-02 20:32:20 +0300

    create books model

---
+++ db/migrate/20220702173027_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :genre
+      t.integer :current_chapter
+      t.integer :total_chapters
+
+      t.timestamps
+    end
+  end
+end
