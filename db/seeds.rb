e49f942d9dee57aea16848f7bdfe587a5b0224b8
daudi13 <davouma13@gmail.com>
2022-07-02 21:31:04 +0300

    hard code books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,76 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+books = Book.create([
+  {
+    title: "In the Heart of the Sea",
+    author: "Moby Dick",
+    genre: "Fantasy",
+    current_chapter: 1,
+    total_chapters: 34
+  },
+  {
+    title: "The Rainmaker",
+    author: "John Grisham",
+    genre: "Legal Fiction",
+    current_chapter: 1,
+    total_chapters: 44
+  },
+  {
+    title: "Watership Down",
+    author: "Richard Adams",
+    genre: "thriller",
+    current_chapter: 1,
+    total_chapters: 66
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Fantasy",
+    current_chapter: 1,
+    total_chapters: 55
+  },
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    current_chapter: 1,
+    total_chapters: 12
+  },
+  {
+    title: "The Da Vinci Code",
+    author: "Dan Brown",
+    genre: "Mystery",
+    current_chapter: 1,
+    total_chapters: 44
+  },
+  {
+    title: "Inferno",
+    author: "Dan Brown",
+    genre: "Mystery",
+    current_chapter: 1,
+    total_chapters: 44
+  },
+  {
+    title: "It",
+    author: "Stephen King",
+    genre: "Horror",
+    current_chapter: 1,
+    total_chapters: 55
+  },
+  {
+    title: "The Shining",
+    author: "Stephen King",
+    genre: "Horror",
+    current_chapter: 1,
+    total_chapters: 45
+  },
+  {
+    title: "The Call of the Wild",
+    author: "Jack London",
+    genre: "Adventure",
+    current_chapter: 1,
+    total_chapters: 55
+  },
+])
