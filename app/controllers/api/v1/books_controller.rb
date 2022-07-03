53f8490862923e75af493174ce26e227b53f36c8
daudi13 <davouma13@gmail.com>
2022-07-03 20:39:11 +0300

    Add show method

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,8 +2,13 @@ module Api
   module V1
     class BooksController < ApplicationController
       def index
-        books = Book.order('created_at DESC');
+        books = Book.order('created_at ASC');
         render json: {status: 'SUCCESS', message:'Loaded books', data:books}, status: :ok
+      end
+
+      def show
+        book = Book.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded book', data:book}, status: :ok
       end
     end
   end
