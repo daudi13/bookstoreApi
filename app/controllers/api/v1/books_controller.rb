1708d925ae7f856f7aac547b2c61a9c6454beec0
daudi13 <davouma13@gmail.com>
2022-07-04 06:47:49 +0300

    Add config.hosts << localhost

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -10,6 +10,22 @@ module Api
         book = Book.find(params[:id])
         render json: {status: 'SUCCESS', message: 'Loaded book', data:book}, status: :ok
       end
+
+      def create
+        book = Book.new(book_params)
+
+        if book.save
+          render json: {status: 'SUCCESS', message: 'Saved book', data:book}, status: :ok
+        else
+          render json: {status: 'ERROR', message:'Book not saved', data:book.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def book_params
+        params.permit(:title, :author, :genre, :current_chapter, :total_chapters)
+      end
     end
   end
 end
